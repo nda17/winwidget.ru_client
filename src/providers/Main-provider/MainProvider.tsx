0dd55e49193938d9e05771366dbaeeaca6695d02
Dmitry <khv1702@gmail.com>
2026-04-09 04:24:45 +0300

    Fix

--- src/providers/Main-provider/MainProvider.tsx
+++ src/providers/Main-provider/MainProvider.tsx
@@ -3,9 +3,17 @@ import CookieConsentProvider from '@/providers/cookie-consent-provider/CookieCon
 import { IMainProvider } from '@/providers/main-provider/main-provider.interface'
 import { NavigationProvider } from '@/providers/navigation-provider/NavigationProvider'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { NextPage } from 'next'
+import dynamic from 'next/dynamic'
 import { Toaster } from 'react-hot-toast'
+
+const ReactQueryDevtools = dynamic(
+	() =>
+		import('@tanstack/react-query-devtools').then(
+			(module) => module.ReactQueryDevtools
+		),
+	{ ssr: false }
+)
 
 const queryClient = new QueryClient({
 	defaultOptions: {
@@ -21,7 +29,9 @@ const MainProvider: NextPage<IMainProvider> = ({ children }) => {
 			<Toaster />
 			<CookieConsentProvider />
 			<NavigationProvider>{children}</NavigationProvider>
-			<ReactQueryDevtools initialIsOpen={false} />
+			{process.env.NODE_ENV === 'development' && (
+				<ReactQueryDevtools initialIsOpen={false} />
+			)}
 		</QueryClientProvider>
 	)
 }
