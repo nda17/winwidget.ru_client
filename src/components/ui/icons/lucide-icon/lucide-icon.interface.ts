3080a920f800b5cf975768162ada8f3d6bc0cf38
Dmitry <khv1702@gmail.com>
2026-04-13 09:11:28 +0300

    Delete component

--- src/components/ui/icons/lucide-icon/lucide-icon.interface.ts
+++
@@ -1,9 +0,0 @@
-import { TypeLucideIconName } from '@/shared/types/icon.types'
-
-export interface ILucideIcon {
-	name: TypeLucideIconName
-	fill?: string
-	color?: string
-	className?: string
-	size?: number
-}
--- src/shared/types/icon.types.ts
+++ src/shared/types/icon.types.ts
@@ -1,7 +1,5 @@
 import * as FontAwesomeIcons from 'react-icons/fa'
-import { icons } from 'lucide-react'
 import * as MaterialIcons from 'react-icons/md'
 
 export type TypeMaterialIconName = keyof typeof MaterialIcons
 export type TypeFontAwesomeName = keyof typeof FontAwesomeIcons
-export type TypeLucideIconName = keyof typeof icons
