0d99af2de9fa635f83ddeda8d5ba639e1af303e2
Dmitry <khv1702@gmail.com>
2026-04-17 22:16:55 +0300

    Add manifest config

---
+++ src/app/manifest.ts
@@ -0,0 +1,37 @@
+import { MetadataRoute } from 'next'
+
+const manifest = (): MetadataRoute.Manifest => {
+	return {
+		name: 'WinWidget — виджеты для вашего сайта',
+		short_name: 'WinWidget',
+		description: 'Продающие виджеты для повышения конверсии сайта',
+		start_url: '/',
+		display: 'standalone',
+		background_color: '#f8f5ff',
+		theme_color: '#470B58',
+		icons: [
+			{
+				src: '/favicon-16x16.png',
+				sizes: '16x16',
+				type: 'image/png'
+			},
+			{
+				src: '/favicon-32x32.png',
+				sizes: '32x32',
+				type: 'image/png'
+			},
+			{
+				src: '/icon-192x192.png',
+				sizes: '192x192',
+				type: 'image/png'
+			},
+			{
+				src: '/icon-512x512.png',
+				sizes: '512x512',
+				type: 'image/png'
+			}
+		]
+	}
+}
+
+export default manifest
