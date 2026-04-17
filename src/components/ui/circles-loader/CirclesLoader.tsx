ecf8d2cf250fc80dcde32387b933321dd1eb042a
Dmitry <khv1702@gmail.com>
2026-04-17 20:31:49 +0300

    Deleted file. Refactor

--- src/components/ui/circles-loader/CirclesLoader.tsx
+++
@@ -1,9 +0,0 @@
-import styles from '@/components/ui/circles-loader/CirclesLoader.module.scss'
-import clsx from 'clsx'
-import { NextPage } from 'next'
-
-const CirclesLoader: NextPage = () => {
-	return <div className={clsx(styles['circles-loader'])}></div>
-}
-
-export default CirclesLoader
