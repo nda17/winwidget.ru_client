19a014a37b608d96fe89aac8d946262e6da045e1
Dmitry <khv1702@gmail.com>
2026-04-17 23:11:12 +0300

    Update routes

--- src/config/pages/public.config.ts
+++ src/config/pages/public.config.ts
@@ -4,15 +4,14 @@ class PublicPages {
 	REGISTER = '/register'
 	RESTORE_PASSWORD = '/restore-password'
 	USER_PROFILE = '/profile'
-	MANAGER = '/manager'
-	FREE_CONTENT = '/free-content'
-	PREMIUM_CONTENT = '/premium-content'
 	PERSONAL_POLICY = '/legal-documentation/personal-policy'
 	CONSENT_PROCESSING = '/legal-documentation/consent-processing'
 	COOKIE_NOTICE = '/legal-documentation/cookie-notice'
 	OFERTA = '/legal-documentation/oferta'
 	SOCIALS_LINK_VK = 'https://vk.ru'
 	SOCIALS_LINK_TG = 'https://web.telegram.org/k/'
+	CABINET = '/cabinet'
+	PAYMENT = '/payment'
 }
 
 export const PUBLIC_PAGES = new PublicPages()
