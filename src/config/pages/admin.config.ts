0c77294b80d6bd9dfacf3347acdfb5d9e98813b2
Dmitry <khv1702@gmail.com>
2026-04-17 23:10:14 +0300

    Add routes

--- src/config/pages/admin.config.ts
+++ src/config/pages/admin.config.ts
@@ -2,6 +2,9 @@ class AdminPages {
 	HOME = '/admin'
 	USER_LIST = '/admin/user-list'
 	USER = '/admin/user'
+	SETTINGS = '/admin/settings'
+	LANDING = '/admin/content'
+	NOTES = '/admin/notes'
 }
 
 export const ADMIN_PAGES = new AdminPages()
