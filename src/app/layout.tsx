a68c3186bcbae82b75710a68561dc1ab224cbfe6
Dmitry <khv1702@gmail.com>
2026-04-20 19:34:44 +0300

    Add local fonts import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import '@/assets/styles/globals.scss'
 import Layout from '@/components/layout/Layout'
 import MainProvider from '@/providers/Main-provider/MainProvider'
 import type { Metadata } from 'next'
-import { Inter, Poppins, Roboto, Unbounded } from 'next/font/google'
+import localFont from 'next/font/local'
 import { PropsWithChildren } from 'react'
 
 export const metadata: Metadata = {
@@ -21,30 +21,115 @@ export const metadata: Metadata = {
 	}
 }
 
-const inter = Inter({
-	subsets: ['latin', 'cyrillic'],
-	weight: ['400', '500', '600', '700', '800'],
+const inter = localFont({
+	src: [
+		{
+			path: '../assets/fonts/inter/Inter-Regular.woff2',
+			weight: '400',
+			style: 'normal'
+		},
+		{
+			path: '../assets/fonts/inter/Inter-Medium.woff2',
+			weight: '500',
+			style: 'normal'
+		},
+		{
+			path: '../assets/fonts/inter/Inter-SemiBold.woff2',
+			weight: '600',
+			style: 'normal'
+		},
+		{
+			path: '../assets/fonts/inter/Inter-Bold.woff2',
+			weight: '700',
+			style: 'normal'
+		},
+		{
+			path: '../assets/fonts/inter/Inter-ExtraBold.woff2',
+			weight: '800',
+			style: 'normal'
+		}
+	],
 	variable: '--font-inter',
 	display: 'swap'
 })
 
-const roboto = Roboto({
-	subsets: ['latin', 'cyrillic'],
-	weight: ['400', '500', '700'],
+const roboto = localFont({
+	src: [
+		{
+			path: '../assets/fonts/roboto/Roboto-Regular.woff2',
+			weight: '400',
+			style: 'normal'
+		},
+		{
+			path: '../assets/fonts/roboto/Roboto-Medium.woff2',
+			weight: '500',
+			style: 'normal'
+		},
+		{
+			path: '../assets/fonts/roboto/Roboto-Bold.woff2',
+			weight: '700',
+			style: 'normal'
+		}
+	],
 	variable: '--font-roboto',
 	display: 'swap'
 })
 
-const unbounded = Unbounded({
-	subsets: ['latin', 'cyrillic'],
-	weight: ['400', '500', '600', '700', '800'],
+const unbounded = localFont({
+	src: [
+		{
+			path: '../assets/fonts/unbounded/Unbounded-Regular.woff2',
+			weight: '400',
+			style: 'normal'
+		},
+		{
+			path: '../assets/fonts/unbounded/Unbounded-Medium.woff2',
+			weight: '500',
+			style: 'normal'
+		},
+		{
+			path: '../assets/fonts/unbounded/Unbounded-SemiBold.woff2',
+			weight: '600',
+			style: 'normal'
+		},
+		{
+			path: '../assets/fonts/unbounded/Unbounded-Bold.woff2',
+			weight: '700',
+			style: 'normal'
+		},
+		{
+			path: '../assets/fonts/unbounded/Unbounded-ExtraBold.woff2',
+			weight: '800',
+			style: 'normal'
+		}
+	],
 	variable: '--font-unbounded',
 	display: 'swap'
 })
 
-const poppins = Poppins({
-	subsets: ['latin'],
-	weight: ['400', '500', '600', '700'],
+const poppins = localFont({
+	src: [
+		{
+			path: '../assets/fonts/poppins/Poppins-Regular.woff2',
+			weight: '400',
+			style: 'normal'
+		},
+		{
+			path: '../assets/fonts/poppins/Poppins-Medium.woff2',
+			weight: '500',
+			style: 'normal'
+		},
+		{
+			path: '../assets/fonts/poppins/Poppins-SemiBold.woff2',
+			weight: '600',
+			style: 'normal'
+		},
+		{
+			path: '../assets/fonts/poppins/Poppins-Bold.woff2',
+			weight: '700',
+			style: 'normal'
+		}
+	],
 	variable: '--font-poppins',
 	display: 'swap'
 })
@@ -56,6 +141,11 @@ const RootLayout = ({ children }: PropsWithChildren<unknown>) => {
 			className={`${inter.variable} ${roboto.variable} ${unbounded.variable} ${poppins.variable}`}
 		>
 			<body>
+				<script
+					src="https://api.winwidget.ru/widgets/wheel.js"
+					data-key="e740f1eadbd4"
+					async
+				></script>
 				<MainProvider>
 					<Layout>{children}</Layout>
 				</MainProvider>
