805af0ad34c4bd471294e7b7f1aa2878a44850da
Dmitry <khv1702@gmail.com>
2026-04-22 01:51:35 +0300

    Refactor, fix

--- src/components/screens/home/hero/HeroSection.tsx
+++ src/components/screens/home/hero/HeroSection.tsx
@@ -1,4 +1,9 @@
+import heroBgDesktopAvif from '@/assets/images/hero/hero-bg-desktop.avif'
+import heroBgDesktopWebp from '@/assets/images/hero/hero-bg-desktop.webp'
+import heroBgMobileAvif from '@/assets/images/hero/hero-bg-mobile.avif'
+import heroBgMobileWebp from '@/assets/images/hero/hero-bg-mobile.webp'
 import Link from 'next/link'
+import type { CSSProperties } from 'react'
 import styles from './HeroSection.module.scss'
 
 interface IHeroSectionProps {
@@ -7,9 +12,19 @@ interface IHeroSectionProps {
 }
 
 const HeroSection = ({ ctaHref, onScrollToFaq }: IHeroSectionProps) => {
+	const heroBackgroundStyle = {
+		'--hero-bg-desktop-webp': `url(${heroBgDesktopWebp.src})`,
+		'--hero-bg-desktop-avif': `url(${heroBgDesktopAvif.src})`,
+		'--hero-bg-mobile-webp': `url(${heroBgMobileWebp.src})`,
+		'--hero-bg-mobile-avif': `url(${heroBgMobileAvif.src})`
+	} as CSSProperties
+
 	return (
 		<div className={styles.heroWrapper}>
-			<div className={styles.heroBackground}></div>
+			<div
+				className={styles.heroBackground}
+				style={heroBackgroundStyle}
+			></div>
 			<div className={styles.heroFirstCircle}></div>
 			<div className={styles.heroSecondCircle}></div>
 			<div className={styles.heroThirdCircle}></div>
