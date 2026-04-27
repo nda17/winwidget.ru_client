a2165dfc574782f3cb18988efb0cdb4e2369b050
Dmitry <khv1702@gmail.com>
2026-04-27 21:40:53 +0300

    Update routes

--- src/config/pages/admin.config.ts
+++ src/config/pages/admin.config.ts
@@ -3,9 +3,10 @@ class AdminPages {
 	USER_LIST = '/admin/user-list'
 	USER = '/admin/user'
 	SETTINGS = '/admin/settings'
-	LANDING = '/admin/content'
-	NOTES = '/admin/notes'
+	CONTENT = '/admin/content'
+	BACKLOG = '/admin/backlog'
 	SUBSCRIPTIONS = '/admin/subscriptions'
+	SYSTEM = '/admin/system'
 }
 
 export const ADMIN_PAGES = new AdminPages()
