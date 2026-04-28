d080f11f1b66681aacda0cd9f660d02df2a8e57b
Dmitry <khv1702@gmail.com>
2026-04-28 04:33:18 +0300

    Add danger zone banner

--- src/components/ui/admin/admin-navigation/AdminNavigation.tsx
+++ src/components/ui/admin/admin-navigation/AdminNavigation.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import AdminDangerBanner from '@/components/ui/admin/admin-danger-banner/AdminDangerBanner'
 import AdminNavItem from '@/components/ui/admin/admin-navigation/AdminNavItem'
 import styles from '@/components/ui/admin/admin-navigation/AdminNavigation.module.scss'
 import { navItems } from '@/components/ui/admin/admin-navigation/data/admin-navigation.data'
@@ -8,13 +9,16 @@ import { NextPage } from 'next'
 
 const AdminNavigation: NextPage = () => {
 	return (
-		<nav aria-label="Навигация администратора">
-			<ul className={clsx(styles['nav-list'])}>
-				{navItems?.map(item => (
-					<AdminNavItem key={item.link} item={item} />
-				))}
-			</ul>
-		</nav>
+		<>
+			<nav aria-label="Навигация администратора">
+				<ul className={clsx(styles['nav-list'])}>
+					{navItems?.map(item => (
+						<AdminNavItem key={item.link} item={item} />
+					))}
+				</ul>
+			</nav>
+			<AdminDangerBanner />
+		</>
 	)
 }
 
